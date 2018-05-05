f83a16886f29d462d4d6da72a5f0eacebff8fe76
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2018-05-05 13:14:01 +0300

    wizard: add manual ssid entry page
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- wizard/wlan.c
+++ wizard/wlan.c
@@ -2,8 +2,14 @@
 #include <connui/iapsettings/wizard.h>
 #include <connui/iapsettings/stage.h>
 #include <connui/iapsettings/mapper.h>
+#include <connui/iapsettings/widgets.h>
 
 #include <string.h>
+#include <libintl.h>
+
+#include "config.h"
+
+#define _(msgid) dgettext(GETTEXT_PACKAGE, msgid)
 
 #define EAP_GTC			6
 #define EAP_TLS			13
@@ -410,6 +416,267 @@ static struct stage_widget iap_wizard_wlan_widgets[] =
 };
 
 static void
+wlan_manual_ssid_entry_changed_cb(GtkEditable *editable,
+                                  wlan_plugin_private *priv)
+{
+  iap_wizard_validate_finish_button(priv->iw);
+}
+
+static void
+wlan_manual_mode_changed_cb(GtkComboBox *widget, wlan_plugin_private *priv)
+{
+  GtkComboBox *wlan_security = GTK_COMBO_BOX(
+        g_hash_table_lookup(priv->plugin->widgets, "WLAN_SECURITY"));
+
+  if (gtk_combo_box_get_active(widget))
+  {
+    if (gtk_combo_box_get_active(wlan_security) > 1)
+      gtk_combo_box_set_active(wlan_security, 0);
+
+    gtk_combo_box_remove_text(wlan_security, 3);
+    gtk_combo_box_remove_text(wlan_security, 2);
+  }
+  else
+  {
+    gtk_combo_box_remove_text(wlan_security, 3);
+    gtk_combo_box_remove_text(wlan_security, 2);
+    gtk_combo_box_append_text(wlan_security,
+                              _("conn_set_iap_fi_wlan_ap_security_wpa_psk"));
+    gtk_combo_box_append_text(wlan_security,
+                              _("conn_set_iap_fi_wlan_ap_security_wpa_eap"));
+  }
+}
+
+static void
+wlan_manual_security_changed_cb(GtkComboBox *widget, wlan_plugin_private *priv)
+{
+  iap_wizard_validate_finish_button(priv->iw);
+}
+
+static GtkWidget *
+wlan_manual_create(gpointer user_data)
+{
+  wlan_plugin_private *priv = user_data;
+  struct iap_wizard *iw = priv->iw;
+  GtkWidget *dialog = iap_wizard_get_dialog(priv->iw);
+  GtkWidget *vbox;
+  GtkSizeGroup *group;
+  HildonGtkInputMode im;
+  GtkWidget *caption;
+  GtkWidget *widget;
+
+  vbox = gtk_vbox_new(FALSE, 0);
+  group = gtk_size_group_new(GTK_SIZE_GROUP_HORIZONTAL);
+
+  /* SSID entry */
+  widget = gtk_entry_new();
+  im = hildon_gtk_entry_get_input_mode(GTK_ENTRY(widget));
+  im &= ~(HILDON_GTK_INPUT_MODE_AUTOCAP | HILDON_GTK_INPUT_MODE_DICTIONARY);
+  hildon_gtk_entry_set_input_mode(GTK_ENTRY(widget), im);
+  g_object_set(widget, "max-length", 32, NULL);
+  g_signal_connect(G_OBJECT(widget), "changed",
+                   G_CALLBACK(wlan_manual_ssid_entry_changed_cb), priv);
+  g_signal_connect(G_OBJECT(widget), "insert_text",
+                   G_CALLBACK(iap_widgets_insert_only_ascii_text), dialog);
+  g_signal_connect(G_OBJECT(widget), "insert-text",
+                   G_CALLBACK(iap_widgets_insert_text_no_8bit_maxval_reach),
+                   dialog);
+  g_hash_table_insert(priv->plugin->widgets, g_strdup("WLAN_SSID"), widget);
+
+  caption = hildon_caption_new(group, _("conn_set_iap_fi_wlan_ap_ssid"),
+                               widget, NULL, HILDON_CAPTION_OPTIONAL);
+  gtk_box_pack_start(GTK_BOX(vbox), caption, FALSE, FALSE, 0);
+
+  /* WLAN hidden check buttton */
+  widget = gtk_check_button_new();
+  g_hash_table_insert(priv->plugin->widgets, g_strdup("WLAN_HIDDEN"), widget);
+  caption = hildon_caption_new(group, _("conn_set_iap_fi_wlan_ap_hidden_ssid"),
+                               widget, NULL, HILDON_CAPTION_OPTIONAL);
+  gtk_box_pack_start(GTK_BOX(vbox), caption, FALSE, FALSE, 0);
+
+  /* WLAN mode combo box */
+  widget = iap_widgets_create_static_combo_box(
+        _("conn_set_iap_fi_wlan_ap_mode_infra"),
+        _("conn_set_iap_fi_wlan_ap_mode_adhoc"),
+        NULL);
+  g_hash_table_insert(priv->plugin->widgets, g_strdup("WLAN_MODE"), widget);
+  caption = hildon_caption_new(group, _("conn_set_iap_fi_wlan_ap_mode"),
+                               widget, NULL, HILDON_CAPTION_OPTIONAL);
+  gtk_box_pack_start(GTK_BOX(vbox), caption, FALSE, FALSE, 0);
+  g_signal_connect(G_OBJECT(widget), "changed",
+                   G_CALLBACK(wlan_manual_mode_changed_cb), priv);
+
+  /* WLAN security combo box */
+  widget = iap_widgets_create_static_combo_box(
+        _("conn_set_iap_fi_wlan_ap_security_none"),
+        _("conn_set_iap_fi_wlan_ap_security_wep"),
+        _("conn_set_iap_fi_wlan_ap_security_wpa_psk"),
+        _("conn_set_iap_fi_wlan_ap_security_wpa_eap"),
+        NULL);
+
+  g_signal_connect(G_OBJECT(widget), "changed",
+                   G_CALLBACK(wlan_manual_security_changed_cb), iw);
+  g_hash_table_insert(priv->plugin->widgets, g_strdup("WLAN_SECURITY"), widget);
+  caption = hildon_caption_new(group, _("conn_set_iap_fi_wlan_ap_security"),
+                               widget,NULL, HILDON_CAPTION_OPTIONAL);
+  gtk_box_pack_start(GTK_BOX(vbox), caption, FALSE, FALSE, 0);
+
+  g_object_unref(G_OBJECT(group));
+
+  return vbox;
+}
+
+static const char *
+wlan_manual_get_page(gpointer user_data, gboolean show_note)
+{
+  wlan_plugin_private *priv = user_data;
+  GtkWidget *widget;
+  const gchar *ssid;
+  const char *result;
+  GtkWidget *dialog;
+
+  dialog = iap_wizard_get_dialog(priv->iw);
+  widget = GTK_WIDGET(g_hash_table_lookup(priv->plugin->widgets, "WLAN_SSID"));
+  ssid = gtk_entry_get_text(GTK_ENTRY(widget));
+
+  if (!GTK_WIDGET_SENSITIVE(widget) || !GTK_WIDGET_PARENT_SENSITIVE(widget) ||
+      (ssid && *ssid))
+  {
+    static const char *page_ids[] =
+                 {"COMPLETE", "WLAN_WEP", "WLAN_WPA_PRESHARED", "WLAN_WPA_EAP"};
+    gint mode = gtk_combo_box_get_active(
+          GTK_COMBO_BOX(g_hash_table_lookup(priv->plugin->widgets,
+                                            "WLAN_MODE")));
+    gint security = gtk_combo_box_get_active(
+          GTK_COMBO_BOX(g_hash_table_lookup(priv->plugin->widgets,
+                                            "WLAN_SECURITY")));
+
+
+    if (mode < 0 || security < 0)
+      return NULL;
+
+    result = page_ids[security];
+  }
+  else
+  {
+    if (show_note)
+    {
+      hildon_banner_show_information(GTK_WIDGET(dialog), NULL,
+                                     _("conn_ib_enter_name"));
+      gtk_widget_grab_focus(widget);
+    }
+
+    return NULL;
+  }
+
+  return result;
+}
+
+struct iap_wizard_page iap_wizard_wlan_pages[] =
+{
+  {
+    "WLAN_MANUAL",
+    "conn_set_iap_ti_wlan_ap",
+    wlan_manual_create,
+    wlan_manual_get_page,
+    NULL,
+    NULL,
+    NULL,
+    "Connectivity_Internetsettings_IAPsetupWLAN",
+    NULL
+  },
+/*  {
+    "WLAN_SCAN",
+    "conn_set_iap_ti_wlan_scanned",
+    wlan_scan_create,
+    wlan_scan_get_page,
+    wlan_scan_finish,
+    wlan_scan_prev,
+    NULL,
+    "Connectivity_Internetsettings_IAPsetupscannedWLANs",
+    NULL
+  },
+  {
+    "WLAN_WEP",
+    "conn_set_iap_ti_wlan_wepkey",
+    wlan_wep_create,
+    wlan_wep_get_page,
+    NULL,
+    NULL,
+    NULL,
+    "Connectivity_Internetsettings_IAPsetupWLANwepkey",
+    NULL
+  },
+  {
+    "WLAN_WPA_PRESHARED",
+    "conn_set_iap_ti_wlan_wpa_psk",
+    wlan_wpa_preshared_create,
+    wlan_wpa_preshared_get_page,
+    NULL,
+    NULL,
+    NULL,
+    "Connectivity_Internetsettings_IAPsetupWLANwpapsk",
+    NULL
+  },
+  {
+    "WLAN_WPA_EAP",
+    "conn_set_iap_ti_wlan_wpa_eap_type",
+    wlan_wpa_eap_create,
+    wlan_wpa_eap_get_page,
+    NULL,
+    NULL,
+    NULL,
+    "Connectivity_Internetsettings_IAPsetupWLANwpaeaptype",
+    NULL
+  },
+  {
+    "WLAN_EAP_TLS",
+    "conn_set_iap_ti_wlan_wpa_eap_tls",
+    wlan_eap_tls_create,
+    NULL,
+    NULL,
+    NULL,
+    "COMPLETE",
+    "Connectivity_Internetsettings_IAPsetupWLANwpaeaptype",
+    NULL
+  },
+  {
+    "WLAN_EAP_PEAP",
+    "conn_set_iap_ti_wlan_wpa_eap_peap",
+    wlan_eap_peap_create,
+    NULL,
+    NULL,
+    NULL,
+    "WLAN_EAP_PASSWORD",
+    "Connectivity_Internetsettings_IAPsetupWLANwpaeaptype",
+    NULL
+  },
+  {
+    "WLAN_EAP_TTLS",
+    "conn_set_iap_ti_wlan_wpa_eap_ttls",
+    wlan_eap_ttls_create,
+    NULL,
+    NULL,
+    NULL,
+    "WLAN_EAP_PASSWORD",
+    "Connectivity_Internetsettings_IAPsetupWLANwpaeaptype",
+    NULL
+  },
+  {
+    "WLAN_EAP_PASSWORD",
+    "conn_set_iap_ti_wlan_wpa_eap_mschapv2",
+    wlan_eap_password_create,
+    NULL,
+    wlan_eap_password_finish,
+    NULL,
+    "COMPLETE",
+    "Connectivity_Internetsettings_IAPsetupWLANwpaeaptype",
+    NULL
+  },*/
+  {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL}
+};
+
+static void
 iap_wizard_wlan_flightmode_status_cb(dbus_bool_t offline, gpointer user_data)
 {
   wlan_plugin_private *priv = user_data;
@@ -448,7 +715,7 @@ iap_wizard_plugin_init(struct iap_wizard *iw,
 
 //  plugin->get_advanced = iap_wizard_wlan_get_advanced;
   plugin->stage_widgets = iap_wizard_wlan_widgets;
-//  plugin->pages = iap_wizard_wlan_pages;
+  plugin->pages = iap_wizard_wlan_pages;
 /*  plugin->get_widgets = iap_wizard_wlan_advanced_get_widgets;
   plugin->advanced_show = iap_wizard_wlan_advanced_show;
   plugin->advanced_done = iap_wizard_wlan_advanced_done;
